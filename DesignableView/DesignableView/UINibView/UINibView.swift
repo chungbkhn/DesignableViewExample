4de30dcbd01aba5c679f25c9ea412f40631490b5
Chung Duong <chunbkhn87@gmail.com>
2018-12-08 16:15:10 +0700

    Init project

---
+++ DesignableView/DesignableView/UINibView/UINibView.swift
@@ -0,0 +1,45 @@
+//
+//  UINibView.swift
+//  DesignableView
+//
+//  Created by David on 12/8/18.
+//  Copyright © 2018 Chung Duong. All rights reserved.
+//
+
+import UIKit
+
+class UINibView: UIView {
+
+	private var view: UIView!
+
+	// MARK: - Inits
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+
+		self.loadFromNib()
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+
+		self.loadFromNib()
+	}
+
+	private func loadFromNib() {
+		let nibName = String(describing: type(of: self))
+		let bundle = Bundle(for: type(of: self))
+		let nib = UINib(nibName: nibName, bundle: bundle)
+
+		guard let view = nib.instantiate(withOwner: self, options: nil).first as? UIView else {
+			return
+		}
+
+		view.frame = bounds
+		view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+		self.view = view
+		self.addSubview(view)
+	}
+
+}
